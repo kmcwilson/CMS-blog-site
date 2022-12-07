b87480b742c21e3ffe21c8cca695ba78e862d3e6
Katie Wilson <kmcwilson4@gmail.com>
2022-12-07 09:35:53 -0500

    organized files and set up handlebars

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars') //Why is this not recognizing express-handlebars?
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controller');
 const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({});
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {   
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-const hbs = exphbs.create({ helpers });
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -20,5 +39,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on: ${PORT}. Visit http://localhost:${PORT} and create an account to start your blog!`
+    )
+  );
 });
